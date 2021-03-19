8e07364802f0fb1bc6dfeebc8ae83b347cfba7ad
Diego Lopes <diego-chez@hotmail.com>
2021-03-19 11:00:27 -0300

    fix table name

--- api/database/migrations/20210319112726-create-enrollments.js
+++ api/database/migrations/20210319112726-create-enrollments.js
@@ -23,7 +23,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'Matriculas',
+          model: 'Turmas',
           key: 'id'
         }
       },
